9eea947b45da08bff3928af68f0ed5d0e1679786
Angel Mavare <angelmavare1@gmail.com>
2022-05-13 21:51:18 -0400

    .

---
+++ src/components/HeroHome.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+<template>
+    <div class="container-fluid">
+        <!-- box-intro -->
+        <section class="box-intro">
+            <div class="table-cell">
+                <h1 class="box-headline letters rotate-2">
+                    <span class="box-words-wrapper">
+                        <b class="is-visible">design.</b>
+                        <b>&nbsp;coding.</b>
+                        <b>graphic.</b>
+                    </span>
+                </h1>
+                <h5>everything you need to build your personal portfolio</h5>
+            </div>
+
+            <div class="mouse">
+                <div class="scroll"></div>
+            </div>
+        </section>
+        <!-- end box-intro -->
+    </div>
+</template>
